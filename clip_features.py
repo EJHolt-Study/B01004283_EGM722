9af7453e54dcb074e1fdf2008024bfce2cbaa9c0
EJHolt-Study <Holt-E1@ulster.ac.uk>
2025-04-26 15:32:58 +0100

    Update clip_features.py

---
+++ clip_features.py
@@ -0,0 +1,24 @@
+import geopandas as gpd
+import pandas as pd
+
+def clip_features(input,overlay):
+    """
+    Clips a GeoDataFrame(GDF) to the geometric extent of another specified GDF.
+    
+    Parameters
+    ----------
+    input: GDF
+        Layer that will be clipped. Can have point, line, or polygon geometry
+    
+    overlay: GDF
+        Layer used to define extent of clipping. Requires polygon geometry
+
+    Returns
+    -------
+    clipped_gdf: new clipped gdf, with input values clipped to geometry extent of overlay
+    """
+    clipped_objects = [] # Creates list to contain clipped objects of input layer
+    clipped_objects = gpd.clip(input,overlay) # Adding clipped input GDF objects to new list
+    clipped_gpd = gpd.GeoDataFrame(pd.concat(clipped_objects, ignore_index=True)) # Creates combined GDF of clipped GDF objects
+
+    return clipped_gpd
